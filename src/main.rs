d0d96f9cea22e796d59ef834d63b414b88e71916
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-07-08 17:26:26 +0800

    Added scroll

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui::{self, RichText, TextStyle};
+use eframe::egui::{self, ScrollArea};
 
 fn main() -> Result<(), eframe::Error> {
     // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
+    let window_size = 640.0;
+
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(640.0, 640.0)),
+        initial_window_size: Some(egui::vec2(window_size, window_size)),
         ..Default::default()
     };
 
@@ -21,14 +23,18 @@ fn main() -> Result<(), eframe::Error> {
             });
 
             ui.horizontal(|ui| {
-                ui.vertical(|ui| {
-                    ui.text_edit_multiline(&mut code);
-                    // Submit button
-                    let submit_button: egui::Response = ui.button("Submit");
-                    if submit_button.clicked() {
-                        println!("stop clicking me uwu");
-                    }
-                });
+                ScrollArea::vertical()
+                    .min_scrolled_height(window_size)
+                    .show(ui, |ui| {
+                        ui.vertical(|ui| {
+                            ui.text_edit_multiline(&mut code);
+                            // Submit button
+                            let submit_button: egui::Response = ui.button("Submit");
+                            if submit_button.clicked() {
+                                println!("stop clicking me uwu");
+                            }
+                        });
+                    });
                 ui.label(format!("your text: {code}"));
             });
         });
